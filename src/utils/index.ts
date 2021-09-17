d16330e308e6cb56e878b077ef6575e4bdb1ef88
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2021-09-17 13:56:36 +0530

    refactor: typo in button

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import { IEntryValue } from "./types";
-
 export function bindEvent(
     element: HTMLElement | Window,
     eventName: keyof HTMLElementEventMap,
@@ -68,19 +66,19 @@ export const createMultipleEditButton = (
 
     multipleEditButton.addEventListener("click", editCallback);
 
-    const multipleExternalLinkButtom = document.createElement("div");
-    multipleExternalLinkButtom.classList.add("cslp-tooltip-child");
-    multipleExternalLinkButtom.setAttribute("data-title", "Go to link");
-    multipleExternalLinkButtom.innerHTML = ` <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    const multipleExternalLinkButton = document.createElement("div");
+    multipleExternalLinkButton.classList.add("cslp-tooltip-child");
+    multipleExternalLinkButton.setAttribute("data-title", "Go to link");
+    multipleExternalLinkButton.innerHTML = ` <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M2.66654 2.66758H13.3332V13.3342H6.66654V16.0009H13.3332C14.0405 16.0009 14.7187 15.72 15.2188 15.2199C15.7189 14.7198 15.9999 14.0415 15.9999 13.3342V2.66758C15.9999 1.96034 15.7189 1.28206 15.2188 0.781964C14.7187 0.281867 14.0405 0.000915527 13.3332 0.000915527H2.66654C1.9593 0.000915527 1.28102 0.281867 0.780927 0.781964C0.280829 1.28206 -0.00012207 1.96034 -0.00012207 2.66758V9.33425H2.66654V2.66758Z" fill="#718096" />
       <path d="M6.94263 7.05734L0.999958 13L2.88529 14.8853L8.82796 8.94267L10.8853 11V5.00001H4.88529L6.94263 7.05734Z" fill="#718096" />
     </svg>`;
 
-    multipleExternalLinkButtom.addEventListener("click", linkCallback);
+    multipleExternalLinkButton.addEventListener("click", linkCallback);
 
     const multipleEditFragment = document.createDocumentFragment();
     multipleEditFragment.appendChild(multipleEditButton);
-    multipleEditFragment.appendChild(multipleExternalLinkButtom);
+    multipleEditFragment.appendChild(multipleExternalLinkButton);
 
     const multipleDiv = document.createElement("div");
     multipleDiv.appendChild(multipleEditFragment);
