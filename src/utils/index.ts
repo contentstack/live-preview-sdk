5ca34915d53b7438cbcedf145e1a515e5e391217
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2022-03-15 13:07:46 +0530

    test: add data-test-id for multi button

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -62,6 +62,10 @@ export const createMultipleEditButton = (
     const multipleExternalLinkButton = document.createElement("div");
     multipleExternalLinkButton.classList.add("cslp-tooltip-child");
     multipleExternalLinkButton.setAttribute("data-title", "Go to link");
+    multipleExternalLinkButton.setAttribute(
+        "data-test-id",
+        "cslp-multiple-external-link-button"
+    );
     multipleExternalLinkButton.innerHTML = ` <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M2.66654 2.66758H13.3332V13.3342H6.66654V16.0009H13.3332C14.0405 16.0009 14.7187 15.72 15.2188 15.2199C15.7189 14.7198 15.9999 14.0415 15.9999 13.3342V2.66758C15.9999 1.96034 15.7189 1.28206 15.2188 0.781964C14.7187 0.281867 14.0405 0.000915527 13.3332 0.000915527H2.66654C1.9593 0.000915527 1.28102 0.281867 0.780927 0.781964C0.280829 1.28206 -0.00012207 1.96034 -0.00012207 2.66758V9.33425H2.66654V2.66758Z" fill="#718096" />
       <path d="M6.94263 7.05734L0.999958 13L2.88529 14.8853L8.82796 8.94267L10.8853 11V5.00001H4.88529L6.94263 7.05734Z" fill="#718096" />
