8be07ee53a74870b216895fe2171d90ff5f4dee5
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2022-04-18 00:10:10 +0530

    chore: do something

--- src/__test__/contentstack-live-preview-HOC.test.ts
+++ src/__test__/contentstack-live-preview-HOC.test.ts
@@ -6,7 +6,7 @@ describe("Live preview HOC Callback Pub Sub", () => {
         ContentstackLivePreview.subscribers = {};
         ContentstackLivePreview.livePreview = null;
     });
-    test("should add function to subscribers when onEntryChange is called", () => {
+    test('should add function to subscribers when onEntryChange is called', () => {
         const onChangeCallback1 = jest.fn();
         const onChangeCallback2 = jest.fn();
         const onChangeCallback3 = jest.fn();
