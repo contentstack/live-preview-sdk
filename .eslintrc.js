efdb9f76066b45ca7fc87ebe900ce9a9f9cfb046
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2022-04-17 23:53:57 +0530

    chore: do something

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier", // This prevents conflicts between eslint and prettier. This should be last
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+        // "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/prefer-const": 0,
+        "@typescript-eslint/no-this-alias": 0,
+        "@typescript-eslint/ban-ts-comment": 0,
+        "prefer-rest-params": 0,
+        "@typescript-eslint/no-unused-vars": 0,
+        "@typescript-eslint/no-empty-function": 0,
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: "lf",
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: "always",
+};
