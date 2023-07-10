941d7bdec6cf9ea715906e36bb4d8e20d478da37
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-07-10 15:29:59 +0530

    feat: Add mode support

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,4 +1,4 @@
-import { IInitData } from "./types";
+import { IConfig, IInitData } from "./types";
 
 export const userInitData: IInitData = {
     ssr: true,
@@ -10,6 +10,8 @@ export const userInitData: IInitData = {
         position: "top",
         includeByQueryParameter: true,
     },
+
+    mode: "preview",
 
     stackDetails: {
         apiKey: "",
@@ -30,3 +32,45 @@ export const userInitData: IInitData = {
     },
     runScriptsOnUpdate: false,
 };
+
+export function getDefaultConfig(): IConfig {
+    return {
+        ssr: true,
+        enable: true,
+        cleanCslpOnProduction: true,
+        editButton: {
+            enable: true,
+            exclude: [],
+            position: "top",
+            includeByQueryParameter: true,
+        },
+
+        mode: 1,
+
+        stackDetails: {
+            apiKey: "",
+            environment: "",
+            contentTypeUid: "",
+            entryUid: "",
+        },
+
+        clientUrlParams: {
+            protocol: "https",
+            host: "app.contentstack.com",
+            port: 443,
+            url: "https://app.contentstack.com:443",
+        },
+        stackSdk: {
+            live_preview: {},
+            headers: {
+                api_key: "",
+            },
+            environment: "",
+        },
+        runScriptsOnUpdate: false,
+
+        onChange() {
+            // this is intentional
+        },
+    };
+}
