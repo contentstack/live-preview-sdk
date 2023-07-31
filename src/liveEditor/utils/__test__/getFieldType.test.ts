7ff3f067b20a28e6a44077b3716464eedd61a5f4
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-07-31 16:06:24 +0530

    feat: update getFieldType

--- src/liveEditor/utils/__test__/getFieldType.test.ts
+++ src/liveEditor/utils/__test__/getFieldType.test.ts
@@ -1,7 +1,18 @@
 import {
+    IBooleanContentTypeSchema,
+    IDateContentTypeSchema,
+    IExperienceContainerContentTypeSchema,
+    IFileContentTypeSchema,
+    IGlobalFieldContentTypeSchema,
+    IGroupContentTypeSchema,
     IHTMLRTEContentTypeSchema,
+    IJSONRTEContentTypeSchema,
+    ILinkContentTypeSchema,
     IMarkdownContentTypeSchema,
+    IModularBlocksContentTypeSchema,
     IMultiLineTextBoxContentTypeSchema,
+    INumberContentTypeSchema,
+    IReferenceContentTypeSchema,
     ISelectContentTypeSchema,
     ISingleLineTextBoxContentTypeSchema,
 } from "../../../types/contentTypeSchema.types";
@@ -118,5 +129,224 @@ describe("getFieldType", () => {
         };
 
         expect(getFieldType(data)).toBe("singleline");
+    });
+
+    test("should return json_rte if it is json_rte", () => {
+        const data: IJSONRTEContentTypeSchema = {
+            data_type: "json",
+            field_metadata: {
+                allow_json_rte: true,
+                embed_entry: false,
+                description: "",
+                default_value: "",
+                multiline: false,
+                rich_text_type: "advanced",
+                options: [],
+            },
+            format: "",
+            error_messages: {
+                format: "",
+            },
+            reference_to: ["sys_assets"],
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("json_rte");
+    });
+
+    test("should return number if it is number", () => {
+        const data: INumberContentTypeSchema = {
+            data_type: "number",
+            field_metadata: {
+                description: "",
+                default_value: "",
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("number");
+    });
+
+    test("should return boolean if it is boolean", () => {
+        const data: IBooleanContentTypeSchema = {
+            data_type: "boolean",
+            field_metadata: {
+                description: "",
+                default_value: false,
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("boolean");
+    });
+
+    test("should return isodate if it is isodate", () => {
+        const data: IDateContentTypeSchema = {
+            data_type: "isodate",
+            startDate: null,
+            endDate: null,
+            field_metadata: {
+                description: "",
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("isodate");
+    });
+
+    test("should return file if it is file", () => {
+        const data: IFileContentTypeSchema = {
+            data_type: "file",
+            extensions: [],
+            field_metadata: {
+                description: "",
+                rich_text_type: "standard",
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("file");
+    });
+
+    test("should return link if it is link", () => {
+        const data: ILinkContentTypeSchema = {
+            data_type: "link",
+            field_metadata: {
+                description: "",
+                default_value: {
+                    title: "",
+                    url: "",
+                },
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("link");
+    });
+
+    test("should return modular_block if it is modular_block", () => {
+        const data: IModularBlocksContentTypeSchema = {
+            data_type: "blocks",
+            blocks: [],
+            field_metadata: {
+                instruction: "",
+                description: "",
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("modular_block");
+    });
+
+    test("should return group if it is group", () => {
+        const data: IGroupContentTypeSchema = {
+            data_type: "group",
+            field_metadata: {
+                description: "",
+                instruction: "",
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+            schema: [],
+        };
+
+        expect(getFieldType(data)).toBe("group");
+    });
+
+    test("should return reference if it is reference", () => {
+        const data: IReferenceContentTypeSchema = {
+            data_type: "reference",
+            reference_to: [],
+            field_metadata: {
+                ref_multiple: false,
+                ref_multiple_content_types: false,
+            },
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("reference");
+    });
+
+    test("should return global_field if it is global_field", () => {
+        const data: IGlobalFieldContentTypeSchema = {
+            data_type: "global_field",
+            reference_to: "",
+            field_metadata: {
+                description: "",
+            },
+            schema: [],
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("global_field");
+    });
+
+    test("should return experience_container if it is experience_container", () => {
+        const data: IExperienceContainerContentTypeSchema = {
+            data_type: "experience_container",
+            field_metadata: {
+                experience_uid: "",
+                project_uid: "",
+                enableDefaultVariation: false,
+            },
+            schema: [],
+            uid: "",
+            display_name: "",
+            mandatory: false,
+            multiple: false,
+            non_localizable: false,
+            unique: false,
+        };
+
+        expect(getFieldType(data)).toBe("experience_container");
     });
 });
--- src/liveEditor/utils/getFieldType.ts
+++ src/liveEditor/utils/getFieldType.ts
@@ -2,28 +2,41 @@
 import { ISchemaFieldMap } from "./types/index.types";
 
 export function getFieldType(fieldSchema: ISchemaFieldMap): string {
-    if (
-        fieldSchema.data_type === "text" &&
-        fieldSchema?.field_metadata?.multiline
-    ) {
-        return "multiline";
-    }
-    if (
-        fieldSchema.data_type === "text" &&
-        fieldSchema?.field_metadata?.allow_rich_text
-    ) {
-        return "html_rte";
-    }
-    if (
-        fieldSchema.data_type === "text" &&
-        fieldSchema?.field_metadata?.markdown
-    ) {
-        return "markdown_rte";
-    }
-    if (fieldSchema.enum) {
-        return "select";
-    }
-    if (fieldSchema.data_type === "text") {
-        return "singleline";
+    switch (fieldSchema.data_type) {
+        case "text": {
+            if (fieldSchema?.field_metadata.multiline) {
+                return "multiline";
+            } else if (fieldSchema?.field_metadata.allow_rich_text) {
+                return "html_rte";
+            } else if (fieldSchema?.field_metadata.markdown) {
+                return "markdown_rte";
+            } else if (fieldSchema.enum) {
+                return "select";
+            } else {
+                return "singleline";
+            }
+        }
+        case "json": {
+            if (fieldSchema.field_metadata.allow_json_rte) {
+                return "json_rte";
+            }
+            break;
+        }
+
+        case "blocks": {
+            return "modular_block";
+        }
+        case "link":
+        case "isodate":
+        case "boolean":
+        case "block":
+        case "number":
+        case "reference":
+        case "group":
+        case "experience_container":
+        case "file":
+        case "global_field": {
+            return fieldSchema.data_type;
+        }
     }
 }
