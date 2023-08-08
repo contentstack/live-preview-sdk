67329e89267725932de6b3430d725922e9cd7b4d
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-08-08 16:11:41 +0530

    fix: update data-csle tag

--- src/liveEditor/utils/constants.ts
+++ src/liveEditor/utils/constants.ts
@@ -1,3 +1,3 @@
 export const numericInputRegex = /^-?\d*(\.\d*)?([eE][-+]?\d*)?$/;
 
-export const LIVE_EDITOR_FIELD_TYPE_ATTRIBUTE_KEY = "data-csle-field-type";
+export const LIVE_EDITOR_FIELD_TYPE_ATTRIBUTE_KEY = "data-cslp-field-type";
