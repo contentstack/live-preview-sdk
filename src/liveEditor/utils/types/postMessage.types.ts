10a90991efc1861ea6e29073b84bca2da72a0742
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-09-22 11:45:25 +0530

    chore: add type for post message events

---
+++ src/liveEditor/utils/types/postMessage.types.ts
@@ -0,0 +1,6 @@
+export enum LiveEditorPostMessageEvents {
+    ADD_INSTANCE = "add-instance",
+    UPDATE_FIELD = "update-field",
+    OPEN_ASSET_MODAL = "open-asset-modal",
+    OPEN_QUICK_FORM = "open-quick-form",
+}
