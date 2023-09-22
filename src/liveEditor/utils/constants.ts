fdac7510e8ba580025f627fc1dd5dc1afa684748
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-09-22 12:19:16 +0530

    feat: add adv post message

--- src/liveEditor/utils/constants.ts
+++ src/liveEditor/utils/constants.ts
@@ -1,3 +1,5 @@
 export const numericInputRegex = /^-?\d*(\.\d*)?([eE][-+]?\d*)?$/;
 
 export const LIVE_EDITOR_FIELD_TYPE_ATTRIBUTE_KEY = "data-cslp-field-type";
+
+export const LIVE_EDITOR_CHANNEL_ID = "live-editor";
---
+++ src/liveEditor/utils/liveEditorPostMessage.ts
@@ -0,0 +1,13 @@
+import { EventManager } from "@contentstack/advanced-post-message";
+import { LIVE_EDITOR_CHANNEL_ID } from "./constants";
+
+let liveEditorPostMessage: EventManager | undefined;
+
+if (typeof window !== "undefined") {
+    liveEditorPostMessage = new EventManager(LIVE_EDITOR_CHANNEL_ID, {
+        target: window.parent,
+        debug: true,
+    });
+}
+
+export default liveEditorPostMessage;
