d604e29a8e39f3bc5ad133a422379546634a62ca
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-12-21 16:29:06 +0530

    feat: move btn to right and add focus event

--- src/liveEditor/utils/types/postMessage.types.ts
+++ src/liveEditor/utils/types/postMessage.types.ts
@@ -4,4 +4,5 @@ export enum LiveEditorPostMessageEvents {
     OPEN_ASSET_MODAL = "open-asset-modal",
     OPEN_QUICK_FORM = "open-quick-form",
     GET_FIELD_SCHEMA = "get-field-schema",
+    FOCUS_FIELD = "focus-field",
 }
