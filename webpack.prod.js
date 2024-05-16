7bb0f33d77c07a0393d4f3292ecadddbf5ea71b9
RavenColEvol <ravilamkoti@gmail.com>
2024-05-16 11:37:43 +0530

    chore: test webpack prod json

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const { DefinePlugin } = require("webpack");
 
 module.exports = merge(common, {
     mode: "production",
+    plugins: [
+        new DefinePlugin({
+            'process.env.PURGE_PREVIEW_SDK': true,
+        })
+    ]
 });
