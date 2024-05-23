4d66f4c18685b649768c613556fef51f2c175f7e
RavenColEvol <ravilamkoti@gmail.com>
2024-05-23 16:42:01 +0530

    fix: custom element extends

--- src/styles/compare.ts
+++ src/styles/compare.ts
@@ -13,16 +13,15 @@ export const loadCompareGlobalStyle = () => {
                 background: rgba(214, 36, 0, 0.2);
                 text-decoration: line-through 2px solid rgba(214, 36, 0, 1);
             }
+        }
+        .cs-compare__void--added {
+            background: rgba(0, 122, 82, 0.2);
+            outline: 2px solid rgba(0, 122, 82);
+        }
 
-            &.cs-compare__void--added {
-                background: rgba(0, 122, 82, 0.2);
-                outline: 2px solid rgba(0, 122, 82);
-            }
-
-            &.cs-compare__void--removed {
-                background: rgba(214, 36, 0, 0.2);
-                outline: 2px solid rgba(214, 36, 0, 1);
-            }
+        .cs-compare__void--removed {
+            background: rgba(214, 36, 0, 0.2);
+            outline: 2px solid rgba(214, 36, 0, 1);
         }
     `;
 };
