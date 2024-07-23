eb0140792649bce819f59068edb2c5e836eca708
devAyushDubey <dubeyayush02@gmail.com>
2024-07-23 16:16:03 +0530

    multiline text field enhancements

--- src/liveEditor/utils/normalizeNonBreakingSpace.ts
+++
@@ -1,4 +0,0 @@
-export function normalizeNonBreakingSpace(text: string): string {
-    // replace non-breaking space with space and normalize all whitespace chars (not \n \r)
-    return text.replace("&nbsp;", " ").replace(/[^\S\r\n]/gm, " ");
-}
