9de5417c335a0ab403723c81e79e6d3f4234545d
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-08-29 17:02:39 +0530

    chore: sync hoc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
 import ContentstackLivePreviewHOC from "./preview/contentstack-live-preview-HOC";
-// import "./styles.css";
+import packageJson from "../package.json";
 
-export const ContentstackLivePreview = ContentstackLivePreviewHOC;
+class LightLivePreviewHoC implements ContentstackLivePreviewHOC {
+    init() {}
+
+    get hash() {
+        return "";
+    }
+
+    onEntryChange(callback: (...args: any[]) => void) {
+        return callback();
+    }
+
+    onLiveEdit() {
+        // intentionally empty
+    }
+
+    unsubscribeOnEntryChange() {
+        // intentionally empty
+    }
+
+    static getSdkVersion(): string {
+        return packageJson.version;
+    }
+}
+
+export const ContentstackLivePreview: ContentstackLivePreviewHOC =
+    process.env.PURGE_PREVIEW_SDK || process.env.REACT_APP_PURGE_PREVIEW_SDK
+        ? LightLivePreviewHoC
+        : ContentstackLivePreviewHOC;
 
 export default ContentstackLivePreview;
-module.exports = ContentstackLivePreview;
