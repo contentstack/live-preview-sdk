59f442b9acfceb2ec7a140b2cb85d9a5e7c8fd68
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2024-09-10 18:50:43 +0530

    feat: add Inter font

--- src/liveEditor/liveEditor.style.ts
+++ src/liveEditor/liveEditor.style.ts
@@ -474,6 +474,8 @@ export function liveEditorStyles() {
 }
 
 export const VisualBuilderGlobalStyles = `
+       @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
+       
        [data-cslp] [contenteditable="true"] {
             outline: none;
         }
