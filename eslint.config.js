e7eaa4e511b478d57b6df70c383388bebe2904c8
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-09-13 14:18:55 +0530

    chore: update eslint to improve DXP
    - show eslint lint issues as warning and not as errors
    - Ignore unused vars issue if it starts with `_`

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,29 @@
 import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import "eslint-plugin-only-warn";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintConfigPrettier,
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            // allow unused vars with underscore prefix
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                },
+            ],
+        },
+    },
 ];
