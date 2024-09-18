959e6b7ff322b3e28639bdcc787d68e870423acd
Venkat <venkat@MacBook-Pro.local>
2024-09-18 16:42:23 +0530

    feat: Handle Comment modal should open on click event in  highlighed icon

--- src/visualBuilder/components/HighlightedCommentIcon.tsx
+++ src/visualBuilder/components/HighlightedCommentIcon.tsx
@@ -2,12 +2,30 @@ import { h } from "preact";
 import { VNode } from "preact";
 import { HighlightCommentIcon } from "./icons";
 import React from "preact/compat";
+import visualBuilderPostMessage from "../utils/visualBuilderPostMessage";
+import { VisualBuilderPostMessageEvents } from "../utils/types/postMessage.types";
+import { IHighlightCommentData } from "../eventManager/useHighlightCommentIcon";
 
-const HighlightedCommentIcon = (): VNode => {
+const HighlightedCommentIcon = (props: {
+    data: IHighlightCommentData;
+}): VNode => {
+    const { data } = props;
+
+    const handleCommentModal = async () => {
+        visualBuilderPostMessage?.send(
+            VisualBuilderPostMessageEvents.OPEN_FIELD_COMMENT_MODAL,
+            {
+                fieldMetadata: data?.fieldMetadata,
+                discussionUID: data?.discussionUID,
+                fieldSchema: data?.fieldSchema,
+                absolutePath: data.absolutePath,
+            }
+        );
+    };
     return (
-        <>
+        <div onClick={handleCommentModal}>
             <HighlightCommentIcon />
-        </>
+        </div>
     );
 };
 
