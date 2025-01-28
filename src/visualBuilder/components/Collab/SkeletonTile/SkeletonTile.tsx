cb791851e9c05620113a71ce1c5c33f257b958ac
MohammedZuhairAhmed <mohammed.ahmed@contentstack.com>
2025-01-28 22:07:12 +0530

    feat(collab): add ui-components and collab feature handling

---
+++ src/visualBuilder/components/Collab/SkeletonTile/SkeletonTile.tsx
@@ -0,0 +1,71 @@
+/** @jsxImportSource preact */
+import React from "preact/compat";
+import { collabStyles } from "../../../visualBuilder.style";
+import classNames from "classnames";
+
+export type SkeletonTileProps = {
+    numberOfTiles: number;
+    tileHeight: number;
+    tileWidth: number | string;
+    tileBottomSpace: number;
+    tileTopSpace: number;
+    tileleftSpace: number;
+    tileRadius?: number;
+    testId?: string;
+};
+
+const SkeletonTile: React.FC<SkeletonTileProps> = (props) => {
+    const {
+        numberOfTiles,
+        tileleftSpace,
+        tileTopSpace,
+        tileHeight,
+        tileBottomSpace,
+        tileWidth,
+        testId,
+        tileRadius = 7,
+    } = props;
+
+    const svgHeight =
+        numberOfTiles * tileHeight +
+        numberOfTiles * tileBottomSpace +
+        numberOfTiles * tileTopSpace;
+
+    const svgWidth =
+        typeof tileWidth === "string" ? tileWidth : tileWidth + tileleftSpace;
+
+    return (
+        <svg
+            data-testid={testId}
+            height={svgHeight}
+            width={svgWidth}
+            className={classNames(
+                "collab-skeletonTileSvgClass",
+                collabStyles()["collab-skeletonTileSvgClass"]
+            )}
+            fill="#EDF1F7"
+        >
+            {Array.from({ length: numberOfTiles }).map((_, index) => (
+                <g key={index}>
+                    <rect
+                        data-testid="rect"
+                        x={tileleftSpace}
+                        y={
+                            index * (tileHeight + tileBottomSpace) +
+                            tileTopSpace
+                        }
+                        rx={tileRadius}
+                        width={tileWidth}
+                        height={tileHeight}
+                    />
+                </g>
+            ))}
+        </svg>
+    );
+};
+
+SkeletonTile.defaultProps = {
+    testId: "collab-skeletonTile",
+} as Partial<SkeletonTileProps>;
+
+export default SkeletonTile;
---
+++ src/visualBuilder/components/Collab/ThreadPopup/loader/ThreadBody.tsx
@@ -0,0 +1,53 @@
+/** @jsxImportSource preact */
+import SkeletonTile from "../../SkeletonTile/SkeletonTile";
+import { collabStyles } from "../../../../visualBuilder.style";
+import classNames from "classnames";
+
+const ThreadBodyLoader = () => {
+    return (
+        <div
+            className={classNames(
+                "collab-thread-body-comment--loader",
+                collabStyles()["collab-thread-body-comment--loader"]
+            )}
+        >
+            <div style={{ display: "flex" }}>
+                <SkeletonTile
+                    numberOfTiles={1}
+                    tileHeight={32}
+                    tileWidth={32}
+                    tileBottomSpace={0}
+                    tileTopSpace={0}
+                    tileleftSpace={0}
+                    tileRadius={50}
+                />
+                <SkeletonTile
+                    numberOfTiles={2}
+                    tileHeight={10}
+                    tileWidth={130}
+                    tileBottomSpace={7}
+                    tileTopSpace={3}
+                    tileleftSpace={10}
+                />
+            </div>
+            <SkeletonTile
+                numberOfTiles={1}
+                tileHeight={14}
+                tileWidth={300}
+                tileBottomSpace={5}
+                tileTopSpace={0}
+                tileleftSpace={0}
+            />
+            <SkeletonTile
+                numberOfTiles={1}
+                tileHeight={14}
+                tileWidth={230}
+                tileBottomSpace={0}
+                tileTopSpace={0}
+                tileleftSpace={0}
+            />
+        </div>
+    );
+};
+
+export default ThreadBodyLoader;
