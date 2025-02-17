834c3e78250f36b32e21c948bda313818f0db7bc
Sairaj Chouhan <sairaj2119@gmail.com>
2025-02-17 19:09:34 +0530

    chore: remove else block

--- src/visualBuilder/utils/getChildrenDirection.ts
+++ src/visualBuilder/utils/getChildrenDirection.ts
@@ -30,24 +30,23 @@ export default function getChildrenDirection(
 
     if (directionFromParentElement && isValidParentDirection) {
         return directionFromParentElement as ValidPositions;
-    } else {
-        const [firstChildElement, secondChildElement, removeClone] =
-            getChildElements(parentElement, parentCslpValue);
-
-        if (!firstChildElement) return "none";
-
-        // get horizontal and vertical position differences
-        const firstChildBounds = firstChildElement.getBoundingClientRect();
-        const secondChildBounds = secondChildElement.getBoundingClientRect();
-
-        const deltaX = Math.abs(firstChildBounds.left - secondChildBounds.left);
-        const deltaY = Math.abs(firstChildBounds.top - secondChildBounds.top);
-
-        const dir = deltaX > deltaY ? "horizontal" : "vertical";
-
-        // remove the clone that was created in case there was only one child
-        removeClone();
-
-        return dir;
     }
+    const [firstChildElement, secondChildElement, removeClone] =
+        getChildElements(parentElement, parentCslpValue);
+
+    if (!firstChildElement) return "none";
+
+    // get horizontal and vertical position differences
+    const firstChildBounds = firstChildElement.getBoundingClientRect();
+    const secondChildBounds = secondChildElement.getBoundingClientRect();
+
+    const deltaX = Math.abs(firstChildBounds.left - secondChildBounds.left);
+    const deltaY = Math.abs(firstChildBounds.top - secondChildBounds.top);
+
+    const dir = deltaX > deltaY ? "horizontal" : "vertical";
+
+    // remove the clone that was created in case there was only one child
+    removeClone();
+
+    return dir;
 }
