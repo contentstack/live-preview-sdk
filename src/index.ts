c42b1cc7641ccdc1bbfa3ae25673bef5447d6c77
MohammedZuhairAhmed <mohammed.ahmed@contentstack.com>
2025-03-04 18:18:06 +0530

    feat(collab): add env based tree-shaking

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,67 @@
 import ContentstackLivePreviewHOC from "./preview/contentstack-live-preview-HOC";
+import { v4 as uuidv4 } from "uuid";
 
 import { IStackSdk as ExternalStackSdkType } from "./types/types";
-export type IStackSdk = ExternalStackSdkType;
-class LightLivePreviewHoC implements ContentstackLivePreviewHOC {
-    static init() {}
 
-    get hash() {
+import {
+    OnEntryChangeCallback,
+    OnEntryChangeCallbackUID,
+    OnEntryChangeConfig,
+} from "./livePreview/types/onEntryChangeCallback.type";
+
+export type IStackSdk = ExternalStackSdkType;
+
+class LightLivePreviewHoC {
+    private static previewConstructors = {};
+    private static onEntryChangeCallbacks = {};
+
+    static init() {
+        if (typeof window === "undefined") {
+            return Promise.resolve(LightLivePreviewHoC.previewConstructors);
+        }
+
+        return LightLivePreviewHoC.initializePreview();
+    }
+
+    private static initializePreview() {
+        LightLivePreviewHoC.previewConstructors = {
+            livePreview: {},
+            visualBuilder: {},
+        };
+
+        LightLivePreviewHoC.onEntryChangeCallbacks = {};
+
+        return Promise.resolve(LightLivePreviewHoC.previewConstructors);
+    }
+
+    static get hash() {
         return "";
     }
 
-    static onEntryChange(callback: (...args: any[]) => void) {
-        return callback();
+    static get config() {
+        return {};
     }
 
-    static onLiveEdit() {
-        // intentionally empty
+    static isInitialized() {
+        return false;
+    }
+
+    static onEntryChange(
+        callback: OnEntryChangeCallback,
+        config: OnEntryChangeConfig = {}
+    ): OnEntryChangeCallbackUID {
+        const { skipInitialRender = false } = config;
+        const callbackUid = uuidv4();
+
+        if (!skipInitialRender) {
+            callback();
+        }
+
+        return callbackUid;
+    }
+
+    static onLiveEdit(callback: OnEntryChangeCallback) {
+        return uuidv4();
     }
 
     static unsubscribeOnEntryChange() {
@@ -26,8 +73,16 @@ class LightLivePreviewHoC implements ContentstackLivePreviewHOC {
     }
 }
 
-// export const ContentstackLivePreview: ContentstackLivePreviewHOC =
-//     ContentstackLivePreviewHOC;
+let ContentstackLivePreview;
+
+if (
+    process.env.PURGE_PREVIEW_SDK === "true" ||
+    process.env.REACT_APP_PURGE_PREVIEW_SDK === "true"
+) {
+    ContentstackLivePreview = LightLivePreviewHoC;
+} else {
+    ContentstackLivePreview = ContentstackLivePreviewHOC;
+}
 
 export const VB_EmptyBlockParentClass = "visual-builder__empty-block-parent";
-export default ContentstackLivePreviewHOC;
+export default ContentstackLivePreview;
