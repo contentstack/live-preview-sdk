187a0a6cfc41229412ed251ff62990cfdbd090ae
MohammedZuhairAhmed <mohammed.ahmed@contentstack.com>
2025-03-05 12:26:45 +0530

    feat(collab): add env based tree-shaking

--- tsup.config.js
+++ tsup.config.js
@@ -3,50 +3,56 @@ import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extens
 import packageJson from './package.json' assert { type: "json" };
 
 export default defineConfig([
-  modernConfig({
+    modernConfig({
     entry: ["src/**/*.ts","src/**/*.tsx", "!src/**/__test__", "!**/*.test.ts"],
-}),
-legacyConfig({
+    }),
+    legacyConfig({
     entry: ["src/**/*.ts","src/**/*.tsx", "!src/**/__test__", "!**/*.test.ts"],
-}),
+    }),
 ])
 
 function modernConfig(opts) {
-  return {
-    entry: opts.entry,
-    define: {
-      'process.env.PACKAGE_VERSION': `"${packageJson.version}"`,
-    },
-    esbuildOptions(options) {
+    return {
+        entry: opts.entry,
+        define: {
+            'process.env.PACKAGE_VERSION': `"${packageJson.version}"`,
+            'process.env.PURGE_PREVIEW_SDK': "process.env.PURGE_PREVIEW_SDK",
+            'process.env.REACT_APP_PURGE_PREVIEW_SDK':
+                "process.env.REACT_APP_PURGE_PREVIEW_SDK",
+        },
+        esbuildOptions(options) {
       options.jsxImportSource = 'preact';
       options.jsx = 'automatic'
-    },      
+        },
     format: ['cjs', 'esm'],
     target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
     outDir: 'dist/modern',
-    dts: true,
-    sourcemap: true,
-    clean: true,
+        dts: true,
+        sourcemap: true,
+        clean: true,
     esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: 'js' })]
   }
 }
 
 function legacyConfig(opts) {
-  return {
-    entry: opts.entry,
-    define: {
-      'process.env.PACKAGE_VERSION': `"${packageJson.version}"`,
-    },
+    return {
+        entry: opts.entry,
+        define: {
+            'process.env.PACKAGE_VERSION': `"${packageJson.version}"`,
+            'process.env.PURGE_PREVIEW_SDK': "process.env.PURGE_PREVIEW_SDK",
+            'process.env.REACT_APP_PURGE_PREVIEW_SDK':
+                "process.env.REACT_APP_PURGE_PREVIEW_SDK",
+        },
     format: ['cjs', 'esm'],
     target: ['es2020', 'node16'],
     outDir: 'dist/legacy',
-    dts: true,
-    sourcemap: true,
-    clean: true,
+        dts: true,
+        sourcemap: true,
+        clean: true,
     esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: 'js' })],
-    esbuildOptions(options) {
+        esbuildOptions(options) {
       options.jsxImportSource = 'preact';
       options.jsx = 'automatic'
-    },
+        },
   }
 }
