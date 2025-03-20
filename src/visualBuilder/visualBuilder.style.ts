11e3862d59cdeb78423cfda371cf29bce499cb2b
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2025-03-20 16:24:47 +0530

    fix: add instance button loading state
    Adding a reference instance is an async operation on visual builder
    side. Use a loading state to disable the button, so that the add
    instance message cannot be sent when it is already sent and a response
    is being awaited

--- src/visualBuilder/visualBuilder.style.ts
+++ src/visualBuilder/visualBuilder.style.ts
@@ -148,6 +148,13 @@ export function visualBuilderStyles() {
             overflow: hidden;
             text-overflow: ellipsis;
         `,
+        "visual-builder__add-button--loading": css`
+            cursor: wait;
+            /* we have not-allowed on disabled, so we need this */
+            &:disabled {
+                cursor: wait;
+            }
+        `,
         "visual-builder__start-editing-btn": css`
             z-index: 1000;
             text-decoration: none;
@@ -666,7 +673,7 @@ export const VisualBuilderGlobalStyles = `
        [data-cslp] [contenteditable="true"] {
             outline: none;
         }
-        
+
         @keyframes visual-builder__spinner {
             0% {
                 transform: rotate(0deg);
