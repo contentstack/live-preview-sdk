ad8eed524f11a97cc79f0c89ae4e55ce80afbc89
MohammedZuhairAhmed <mohammed.ahmed@contentstack.com>
2025-03-24 15:59:17 +0530

    fix(collab): addressed PR comments

--- src/visualBuilder/components/Collab/Tooltip/Tooltip.tsx
+++ src/visualBuilder/components/Collab/Tooltip/Tooltip.tsx
@@ -2,6 +2,7 @@
 import React from "preact/compat";
 import { useState, useRef, useEffect } from "preact/hooks";
 import { collabStyles } from "../../../collab.style";
+import { positionTooltip } from "../../../utils/collabUtils";
 import classNames from "classnames";
 
 interface TooltipProps {
@@ -50,115 +51,16 @@ const Tooltip = (props: TooltipProps): JSX.Element => {
     }, [children]);
 
     useEffect(() => {
-        const positionTooltip = (): void => {
-            if (!isVisible || !tooltipRef.current || !targetRef.current) return;
+        const updateTooltip = () =>
+            positionTooltip(tooltipRef, targetRef, position, setActualPosition);
 
-            const targetRect: DOMRect =
-                targetRef.current.getBoundingClientRect();
-            const tooltipRect: DOMRect =
-                tooltipRef.current.getBoundingClientRect();
-            const margin: number = 8;
-
-            const positions: Positions = {
-                bottom: {
-                    top: targetRect.bottom + margin,
-                    left:
-                        targetRect.left +
-                        (targetRect.width - tooltipRect.width) / 2,
-                },
-                top: {
-                    top: targetRect.top - tooltipRect.height - margin,
-                    left:
-                        targetRect.left +
-                        (targetRect.width - tooltipRect.width) / 2,
-                },
-                left: {
-                    top:
-                        targetRect.top +
-                        (targetRect.height - tooltipRect.height) / 2,
-                    left: targetRect.left - tooltipRect.width - margin,
-                },
-                right: {
-                    top:
-                        targetRect.top +
-                        (targetRect.height - tooltipRect.height) / 2,
-                    left: targetRect.right + margin,
-                },
-            };
-
-            let bestPosition = position;
-            let coords = positions[position];
-
-            const viewportWidth = window.innerWidth;
-            const viewportHeight = window.innerHeight;
-
-            const wouldBeOutsideViewport = {
-                bottom: coords.top + tooltipRect.height > viewportHeight,
-                top: coords.top < 0,
-                left: coords.left < 0,
-                right: coords.left + tooltipRect.width > viewportWidth,
-            };
-
-            const horizontalOutOfBounds =
-                coords.left < 0 ||
-                coords.left + tooltipRect.width > viewportWidth;
-
-            if (wouldBeOutsideViewport[position] || horizontalOutOfBounds) {
-                const positionPriority = ["bottom", "top", "right", "left"];
-
-                positionPriority.splice(positionPriority.indexOf(position), 1);
-                positionPriority.push(position);
-
-                for (const pos of positionPriority) {
-                    const testCoords = positions[pos as keyof Positions];
-
-                    const isVisible =
-                        testCoords.top >= 0 &&
-                        testCoords.top + tooltipRect.height <= viewportHeight &&
-                        testCoords.left >= 0 &&
-                        testCoords.left + tooltipRect.width <= viewportWidth;
-
-                    if (isVisible) {
-                        bestPosition = pos as
-                            | "top"
-                            | "bottom"
-                            | "left"
-                            | "right";
-                        coords = testCoords;
-                        break;
-                    }
-                }
-            }
-
-            if (coords.left < 0) {
-                coords.left = margin;
-            } else if (coords.left + tooltipRect.width > viewportWidth) {
-                coords.left = viewportWidth - tooltipRect.width - margin;
-            }
-
-            if (coords.top < 0) {
-                coords.top = margin;
-            } else if (coords.top + tooltipRect.height > viewportHeight) {
-                coords.top = viewportHeight - tooltipRect.height - margin;
-            }
-
-            setActualPosition(
-                bestPosition as "top" | "bottom" | "left" | "right"
-            );
-
-            Object.assign(tooltipRef.current.style, {
-                top: `${coords.top}px`,
-                left: `${coords.left}px`,
-            });
-        };
-
-        positionTooltip();
-        window.addEventListener("scroll", positionTooltip);
-        window.addEventListener("resize", positionTooltip);
+        updateTooltip();
+        window.addEventListener("scroll", updateTooltip);
+        window.addEventListener("resize", updateTooltip);
 
         return () => {
-            window.removeEventListener("scroll", positionTooltip);
-            window.removeEventListener("resize", positionTooltip);
+            window.removeEventListener("scroll", updateTooltip);
+            window.removeEventListener("resize", updateTooltip);
         };
     }, [isVisible, position]);
 
