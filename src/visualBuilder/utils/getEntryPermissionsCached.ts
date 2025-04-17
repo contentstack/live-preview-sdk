c4711b4527982766d480b294af9b9d7bf2e4c8f3
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2025-04-17 17:59:21 +0530

    test: createCachedFetch

--- src/visualBuilder/utils/getEntryPermissionsCached.ts
+++ src/visualBuilder/utils/getEntryPermissionsCached.ts
@@ -1,7 +1,6 @@
 import { getEntryPermissions } from "./getEntryPermissions";
 import { createCachedFetch } from "./createCachedFetch";
 
-console.log("Loading module: getEntryPermissionsCached [original]");
 export const getEntryPermissionsCached = createCachedFetch(
     getEntryPermissions,
     ({ entryUid, contentTypeUid, locale }) =>
