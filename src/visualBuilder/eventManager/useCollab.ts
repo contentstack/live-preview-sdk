0f8dcf1df32a52ffff9e1a168200c28f085bee2f
diwakarmk7 <diwakar.mk@contentstack.com>
2025-05-19 12:55:13 +0530

    popup state fix

--- src/visualBuilder/eventManager/useCollab.ts
+++ src/visualBuilder/eventManager/useCollab.ts
@@ -43,6 +43,10 @@ export const useCollab = () => {
                     "collab.pauseFeedback",
                     data?.data?.collab?.pauseFeedback
                 );
+                Config.set(
+                    "collab.isFeedbackMode",
+                    data?.data?.collab?.isFeedbackMode
+                );
                 showAllCollabIcons();
                 return;
             }
