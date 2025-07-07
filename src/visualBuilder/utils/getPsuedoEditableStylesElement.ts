f28d629d362d5820b8583f748b42bd98d464c180
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2025-07-07 15:05:50 +0530

    fix: psuedo-editable height collapse
    The height of the cloned/psuedo-editable element collapses when
    text-wrap-mode is set to nowrap and text-overflow is set to ellipsis.
    This change ensures that we override these properties CORRECTLY.
    Previously we overrode these properties but only in camel case. Now we
    ensure we perform the camelCase transformation after overriding
    properties. We also set min-height to whatever the current height of the
    element is, to prevent height collapse in other scenarios.

--- src/visualBuilder/utils/getPsuedoEditableStylesElement.ts
+++ src/visualBuilder/utils/getPsuedoEditableStylesElement.ts
@@ -6,9 +6,6 @@ export function getPsuedoEditableElementStyles(
     camelCase?: boolean
 ): { [key: string]: string } {
     let styles = getStyleOfAnElement(psuedoEditableElement);
-    if (camelCase) {
-        styles = getCamelCaseStyles(styles);
-    }
     // Get the offsetTop and offsetLeft of the editable element and set the position of the pseudo editable element
     // The pseudo editable element is positioned absolutely at the same location as the editable element
     const rect = psuedoEditableElement.getBoundingClientRect();
@@ -19,8 +16,14 @@ export function getPsuedoEditableElementStyles(
     // setting height to auto so that the element can grow based on the content
     // and the resize observer can detect the change in height
     styles.height = "auto";
-    styles.whiteSpace = "pre-line";
-    styles.textTransform = "none";
+    styles["min-height"] = `${Math.abs(rect.height)}px`;
+    styles["white-space"] = "normal";
+    styles["text-transform"] = "none";
+    styles["text-wrap-mode"] = "wrap";
+    styles["text-overflow"] = "visible";
 
+    if (camelCase) {
+        styles = getCamelCaseStyles(styles);
+    }
     return styles;
 }
