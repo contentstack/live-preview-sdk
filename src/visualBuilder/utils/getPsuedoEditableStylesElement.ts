e8388d05c49464ab3352cd566ca9a1976ff5f6b1
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2025-07-07 17:48:11 +0530

    fix: extract the overriding function and improve unit tests

--- src/visualBuilder/utils/getPsuedoEditableStylesElement.ts
+++ src/visualBuilder/utils/getPsuedoEditableStylesElement.ts
@@ -1,4 +1,5 @@
 import getCamelCaseStyles from "./getCamelCaseStyles";
+import { getPsuedoEditableEssentialStyles } from "./getPsuedoEditableEssentialStyles";
 import getStyleOfAnElement from "./getStyleOfAnElement";
 
 export function getPsuedoEditableElementStyles(
@@ -10,20 +11,9 @@ export function getPsuedoEditableElementStyles(
     // The pseudo editable element is positioned absolutely at the same location as the editable element
     const rect = psuedoEditableElement.getBoundingClientRect();
 
-    styles.position = "absolute";
-    styles.top = `${rect.top + window.scrollY}px`;
-    styles.left = `${rect.left + window.scrollX}px`;
-    // setting height to auto so that the element can grow based on the content
-    // and the resize observer can detect the change in height
-    styles.height = "auto";
-    styles["min-height"] = `${Math.abs(rect.height)}px`;
-    styles["white-space"] = "normal";
-    styles["text-transform"] = "none";
-    styles["text-wrap-mode"] = "wrap";
-    styles["text-overflow"] = "visible";
-
     if (camelCase) {
         styles = getCamelCaseStyles(styles);
     }
-    return styles;
+    const overrides = getPsuedoEditableEssentialStyles({ rect, camelCase });
+    return { ...styles, ...overrides };
 }
