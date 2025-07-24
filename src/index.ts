f70131a824f5e27d10887f91a70a694e197e990d
Sairaj Chouhan <sairaj2119@gmail.com>
2025-07-24 13:31:06 +0530

    Revert "fix: undo unneccsary changes"
    This reverts commit bf2cc68b83f3bb4889c3ecc497b6e77d785ee001.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import LightLivePreviewHoC from "./light-sdk";
 
 export type IStackSdk = ExternalStackSdkType;
 
-
 const ContentstackLivePreview =
     typeof process !== "undefined" &&
     (process?.env?.PURGE_PREVIEW_SDK === "true" ||
